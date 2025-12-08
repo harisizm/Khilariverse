907cd10be257fa1c6995d21050e01542d991d2d1
Haris Iftikhar <haris687.chohan@gmail.com>
2025-12-08 11:58:52 +0500

    backend setup: routes, database models

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+//routes
+import authRoutes from "./routes/auth.route.js";
+
+//db
+import { connectDB } from './lib/db.js';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//authentication
+
+//use routes here
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+  console.log('Server is running on http://localhost:'+PORT);
+
+  connectDB();
+})
--- notes.md
+++ notes.md
@@ -2,3 +2,12 @@
 
 
 react router and react icon libraries used 
+
+dot env is for reading env file 
+bcrypt for hashing passwords
+
+CREATE ROUTES and controllers
+
+> always use .js when importing something from local file
+
+connect db 
